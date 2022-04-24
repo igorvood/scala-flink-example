b0fe8f2a25b8497662694e275bd118dacfab26de
IBalitskiy <IBalitskiy@inno.tech>
2022-04-24 18:12:05 +0300

    init

--- src/test/scala/ru/vood/flink/simple/test/SimpleFlinkKafkaTest.scala
+++ src/test/scala/ru/vood/flink/simple/test/SimpleFlinkKafkaTest.scala
@@ -1,16 +1,16 @@
 package ru.vood.flink.simple.test
 
-import java.util.Properties
-
 import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer011, FlinkKafkaProducer011}
-import org.scalatest.matchers.should.{Matchers, WordSpec}
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer, FlinkKafkaProducer}
+import org.scalatest.matchers.should
 
+import java.util.Properties
 import scala.collection.mutable.ListBuffer
+
 
 object SimpleFlinkKafkaTest {
 
@@ -36,3 +36,47 @@ object SimpleFlinkKafkaTest {
   val inputString = "mystring"
   val expectedString = "MYSTRING"
 }
+
+class SimpleFlinkKafkaTest extends org.scalatest.flatspec.AnyFlatSpec with should.Matchers with EmbeddedKafka {
+
+  "runs with embedded kafka" should "work" in {
+
+    implicit val config = EmbeddedKafkaConfig(
+      kafkaPort = SimpleFlinkKafkaTest.kafkaPort,
+      zooKeeperPort = SimpleFlinkKafkaTest.zooKeeperPort
+    )
+
+    withRunningKafka {
+
+      publishStringMessageToKafka("input-topic", SimpleFlinkKafkaTest.inputString)
+
+      val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+      env.setParallelism(1)
+
+      val kafkaConsumer = new FlinkKafkaConsumer(
+        "input-topic",
+        new SimpleStringSchema,
+        SimpleFlinkKafkaTest.props
+      )
+
+      implicit val typeInfo = TypeInformation.of(classOf[String])
+
+      val inputStream = env.addSource(kafkaConsumer)
+
+      val outputStream = inputStream.map(_.toUpperCase)
+
+      val kafkaProducer = new FlinkKafkaProducer(
+        "output-topic",
+        new SimpleStringSchema(),
+        SimpleFlinkKafkaTest.props
+      )
+      outputStream.addSink(kafkaProducer)
+      env.execute()
+      consumeFirstStringMessageFrom("output-topic") shouldEqual SimpleFlinkKafkaTest.expectedString
+
+    }
+
+  }
+}
+
