3b68e9a32a46d550b3d3d7f49dfaca84706e6d5d
IBalitskiy <IBalitskiy@inno.tech>
2022-04-26 20:27:06 +0300

    init

--- src/test/scala/ru/vood/flink/configuration/AllApplicationPropertiesTest.scala
+++ src/test/scala/ru/vood/flink/configuration/AllApplicationPropertiesTest.scala
@@ -5,15 +5,30 @@ import ru.vood.flink.configuration.example.{KafkaProducerPropertyMap, KafkaPrope
 class AllApplicationPropertiesTest extends org.scalatest.flatspec.AnyFlatSpec {
 
 
+  private val producerPrefix = "kafka.producer.custom"
   "read property" should " be success " in {
 
     implicit val properties: AllApplicationProperties = ConfigUtils.getAllProps(fileNames = List("KafkaProducerPropertyMap.properties"))
 
     val kafkaProperty = KafkaProperty("kafka.producer.common")
 
-    val kafkaProducerPropertyMap = KafkaProducerPropertyMap("kafka.producer.custom", kafkaProperty)
+    val kafkaProducerPropertyMap = KafkaProducerPropertyMap(producerPrefix, kafkaProperty)
 
-    println(kafkaProperty)
+    assertResult(2)(kafkaProducerPropertyMap.producers.size)
+
+  }
+
+  "read property" should " be error " in {
+
+    val properties: AllApplicationProperties = ConfigUtils.getAllProps(fileNames = List("KafkaProducerPropertyMap.properties"))
+
+    val bad = properties.prop.filter(prp => !prp._1.contains(producerPrefix))
+    implicit val baseLocalApplicationProperties: AllApplicationProperties = AllApplicationProperties(bad)
+
+    val kafkaProperty = KafkaProperty("kafka.producer.common")
+
+    val assertion = assertThrows[IllegalArgumentException](KafkaProducerPropertyMap(producerPrefix, kafkaProperty))
+
   }
 
 }
