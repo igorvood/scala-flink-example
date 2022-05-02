211d514f1d3ef6e05c1df3d973674f7e4b934ded
IBalitskiy <IBalitskiy@inno.tech>
2022-05-02 22:32:26 +0300

    init

--- src/main/scala/ru/vood/flink/job/FlinkJobConfiguration.scala
+++ src/main/scala/ru/vood/flink/job/FlinkJobConfiguration.scala
@@ -1,5 +1,13 @@
 package ru.vood.flink.job
 
-case class FlinkJobConfiguration() {
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import ru.vood.flink.configuration.example.KafkaConsumerProperty
+import ru.vood.flink.dto.UniversalDto
+import ru.vood.flink.kafka.consumer.ConsumerFactory.{des, getKafkaConsumer}
+import ru.vood.flink.avro.AvroUtil._
 
+case class FlinkJobConfiguration(kafkaConsumerProperty: KafkaConsumerProperty
+                                ) {
+
+  lazy val kafkaConsumer: FlinkKafkaConsumer[UniversalDto] = getKafkaConsumer[UniversalDto](kafkaConsumerProperty)
 }
