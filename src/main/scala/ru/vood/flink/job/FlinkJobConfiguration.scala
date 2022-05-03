5f802bfe901837e9aae10993078700b72412cb2c
IBalitskiy <IBalitskiy@inno.tech>
2022-05-03 18:38:34 +0300

    init

--- src/main/scala/ru/vood/flink/job/FlinkJobConfiguration.scala
+++ src/main/scala/ru/vood/flink/job/FlinkJobConfiguration.scala
@@ -3,11 +3,12 @@ package ru.vood.flink.job
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import ru.vood.flink.avro.AvroUtil._
 import ru.vood.flink.configuration.AllApplicationProperties
-import ru.vood.flink.configuration.example.{KafkaCnsProperty, KafkaConsumerProperty}
+import ru.vood.flink.configuration.example.{KafkaCnsProperty, KafkaConsumerProperty, KafkaPrdProperty, KafkaProducerPropertyMap}
 import ru.vood.flink.dto.UniversalDto
 import ru.vood.flink.kafka.consumer.ConsumerFactory.{des, getKafkaConsumer}
 
-case class FlinkJobConfiguration(kafkaConsumerProperty: KafkaConsumerProperty
+case class FlinkJobConfiguration(kafkaConsumerProperty: KafkaConsumerProperty,
+                                 kafkaProducerPropertyMap: KafkaProducerPropertyMap
                                 ) {
 
   lazy val kafkaConsumer: FlinkKafkaConsumer[UniversalDto] = getKafkaConsumer[UniversalDto](kafkaConsumerProperty)
@@ -15,14 +16,17 @@ case class FlinkJobConfiguration(kafkaConsumerProperty: KafkaConsumerProperty
 
 
 object FlinkJobConfiguration {
-  val consumerPrefix = "app.kafka.consumer."
+  val consumerPrefix: String = "app.kafka.consumer."
+  val producerPrefix: String = "app.kafka.producers."
 
   def apply(implicit properties: AllApplicationProperties): FlinkJobConfiguration = {
 
     val consumerProperty = KafkaCnsProperty(consumerPrefix + "property.")
+    val producerProperty = KafkaPrdProperty(producerPrefix + "property.")
 
     new FlinkJobConfiguration(
-      KafkaConsumerProperty.apply(consumerPrefix, consumerProperty)
+      kafkaConsumerProperty = KafkaConsumerProperty.apply(consumerPrefix, consumerProperty),
+      kafkaProducerPropertyMap = KafkaProducerPropertyMap(producerPrefix, producerProperty)
     )
   }
 }
