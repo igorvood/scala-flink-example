cdacbbe802952d3ac2591c44ac0054dfac7a620e
IBalitskiy <IBalitskiy@inno.tech>
2022-05-07 12:26:05 +0300

    init

---
+++ flink-gutling-test/src/test/scala/ru/vood/flink/gatling/UaspStreamingBussinesRulesIntegrationTestScript.scala
@@ -0,0 +1,13 @@
+package ru.vood.flink.gatling
+
+import io.gatling.core.scenario.Simulation
+
+class UaspStreamingBussinesRulesIntegrationTestScript/* extends Simulation*/ {
+
+
+  /*
+    setUp(
+      scenarioBusinessRules.inject(atOnceUsers(COUNT_USERS)).protocols(kafkaConf)
+    )
+  */
+}
