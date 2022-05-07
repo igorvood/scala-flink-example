193e401c91195d345c8c7601d302af7cbb1fca94
IBalitskiy <IBalitskiy@inno.tech>
2022-05-07 12:59:29 +0300

    init

--- flink-gutling-test/src/test/scala/ru/vood/flink/gatling/base/IDEPathHelper.scala
+++ flink-gutling-test/src/test/scala/ru/vood/flink/gatling/base/IDEPathHelper.scala
@@ -8,9 +8,9 @@ object IDEPathHelper {
   private val mavenTargetDirectory = projectRootDir.resolve("target")
   private val mavenSrcTestDirectory = projectRootDir.resolve("src").resolve("test")
 
-  val mavenSourcesDirectory: Path = mavenSrcTestDirectory.resolve("scala")
+//  val mavenSourcesDirectory: Path = mavenSrcTestDirectory.resolve("scala")
   val mavenResourcesDirectory: Path = mavenSrcTestDirectory.resolve("resources")
   val mavenBinariesDirectory: Path = mavenTargetDirectory.resolve("test-classes")
   val resultsDirectory: Path = mavenTargetDirectory.resolve("gatlingReport")
-  val recorderConfigFile: Path = mavenResourcesDirectory.resolve("recorder.conf")
+//  val recorderConfigFile: Path = mavenResourcesDirectory.resolve("recorder.conf")
 }
