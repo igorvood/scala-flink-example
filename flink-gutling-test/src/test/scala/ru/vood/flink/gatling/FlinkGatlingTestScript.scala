2bc06c285e930555957f7387df92fea4ad8f0f8e
IBalitskiy <IBalitskiy@inno.tech>
2022-05-08 14:57:07 +0300

    init

--- flink-gutling-test/src/test/scala/ru/vood/flink/gatling/FlinkGatlingTestScript.scala
+++ flink-gutling-test/src/test/scala/ru/vood/flink/gatling/FlinkGatlingTestScript.scala
@@ -1,79 +1,12 @@
 package ru.vood.flink.gatling
 
-import com.github.mnogu.gatling.kafka.Predef.kafka
-import com.github.mnogu.gatling.kafka.protocol.KafkaProtocol
-import com.sksamuel.avro4s.{AvroSchema, Encoder}
-import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
-import io.gatling.core.session.Session
-import org.apache.avro.Schema
-import org.apache.avro.generic.{GenericDatumWriter, GenericRecord}
-import ru.vood.flink.avro.AvroUtil
-import ru.vood.flink.dto.UniversalDto
-import ru.vood.flink.gatling.common.FooCounter
-import ru.vood.flink.gatling.config.{AdditionalProducerGatlingProp, FlinkGatlingConfig, GenerationParameters}
-import ru.vood.flink.gatling.constructor.impl.KafkaProtocolCreator.GatlingKafkaProtocolPreDef
+import ru.vood.flink.gatling.config.FlinkGatlingConfig
 import ru.vood.flink.gatling.constructor.scenario.SimpleScenario
-
-import java.util.Calendar
-import scala.collection.mutable
-import scala.util.Random
 
 class FlinkGatlingTestScript extends Simulation {
 
   implicit val config: FlinkGatlingConfig = FlinkGatlingConfig.apply()
-
-  def startUserNumberFrom(): Long = Random.nextInt(100) * Random.nextInt(100) * 10000
-
-  val START_USERS: Long = startUserNumberFrom()
-  private val generationParam: GenerationParameters = config.generationParam
-  val prefix: String = generationParam.prefixIdentity.getOrElse("")
-  val fooCounter = new FooCounter(START_USERS)
-  val feeder: Iterator[Map[String, Any]] = Iterator.continually(generateMsgFields)
-  var expectedResultsMap = mutable.Map[String, UniversalDto]();
-
-  def generateMsgFields(): Map[String, Any] = {
-    val time = Calendar.getInstance().getTime.getTime
-
-    Map(
-      "uuid" -> java.util.UUID.randomUUID().toString,
-      "process_timestamp" -> time,
-      "expectedResultsMap" -> expectedResultsMap,
-      "operation_id" -> null
-    )
-  }
-
-  val schema: Schema = AvroSchema[UniversalDto]
-  val encoder: Encoder[UniversalDto] = com.sksamuel.avro4s.Encoder[UniversalDto]
-  val writer = new GenericDatumWriter[GenericRecord](schema)
-
-
-  /*
-
-
-    def generateMsgFields(): Map[String, Any] = {
-      val time = Calendar.getInstance().getTime.getTime
-
-      Map(
-        "uuid" -> java.util.UUID.randomUUID().toString,
-        "process_timestamp" -> time,
-        "expectedResultsMap" -> expectedResultsMap,
-        "operation_id" -> null
-      )
-    }
-
-
-
-
-
-    def startUserNumberFrom(): Long = Random.nextInt(100) * Random.nextInt(100) * 10000
-
-    val START_USERS: Long = startUserNumberFrom()
-    val fooCounter = new FooCounter(START_USERS)
-
-  */
-
-
 
   setUp(
     SimpleScenario().createPopulationBuilder
