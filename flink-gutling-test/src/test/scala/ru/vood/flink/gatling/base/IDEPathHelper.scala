476258b554a06e3b1cf3847e1da5b911ec4c6219
IBalitskiy <IBalitskiy@inno.tech>
2022-05-08 17:42:53 +0300

    init

--- flink-gutling-test/src/test/scala/ru/vood/flink/gatling/base/IDEPathHelper.scala
+++ flink-gutling-test/src/test/scala/ru/vood/flink/gatling/base/IDEPathHelper.scala
@@ -4,13 +4,12 @@ import java.nio.file.{Path, Paths}
 
 object IDEPathHelper {
 
-  private val projectRootDir = Paths.get(getClass.getClassLoader.getResource("gatling.conf").toURI).getParent.getParent.getParent
-  private val mavenTargetDirectory = projectRootDir.resolve("target")
-  private val mavenSrcTestDirectory = projectRootDir.resolve("src").resolve("test")
-
-//  val mavenSourcesDirectory: Path = mavenSrcTestDirectory.resolve("scala")
-  val mavenResourcesDirectory: Path = mavenSrcTestDirectory.resolve("resources")
-  val mavenBinariesDirectory: Path = mavenTargetDirectory.resolve("test-classes")
-  val resultsDirectory: Path = mavenTargetDirectory.resolve("gatlingReport")
-//  val recorderConfigFile: Path = mavenResourcesDirectory.resolve("recorder.conf")
+  //  val mavenSourcesDirectory: Path = mavenSrcTestDirectory.resolve("scala")
+  lazy val mavenResourcesDirectory: Path = mavenSrcTestDirectory.resolve("resources")
+  lazy val mavenBinariesDirectory: Path = mavenTargetDirectory.resolve("test-classes")
+  lazy val resultsDirectory: Path = mavenTargetDirectory.resolve("gatlingReport")
+  private lazy val projectRootDir = Paths.get(getClass.getClassLoader.getResource("gatling.conf").toURI).getParent.getParent.getParent
+  private lazy val mavenTargetDirectory = projectRootDir.resolve("target")
+  private lazy val mavenSrcTestDirectory = projectRootDir.resolve("src").resolve("test")
+  //  val recorderConfigFile: Path = mavenResourcesDirectory.resolve("recorder.conf")
 }
