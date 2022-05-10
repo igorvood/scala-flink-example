baf184b71be713c5da5cb8540884f181ba162d05
IBalitskiy <IBalitskiy@inno.tech>
2022-05-10 13:49:51 +0300

    init

--- flink-gutling-test/src/test/scala/ru/vood/flink/gatling/constructor/abstractscenario/layer/first/GatlingScenarioBuilder.scala
+++ flink-gutling-test/src/test/scala/ru/vood/flink/gatling/constructor/abstractscenario/layer/first/GatlingScenarioBuilder.scala
@@ -43,7 +43,7 @@ trait GatlingScenarioBuilder[DTO_IN] extends SessionParamNames with GatlingScena
 
   }
 
-  def idGenerateActionBuilder(session: Session)(implicit generationParameters: GenerationParameters): Session = {
+  protected def idGenerateActionBuilder(session: Session)(implicit generationParameters: GenerationParameters): Session = {
     val prefix = generationParameters.prefixIdentity.getOrElse("")
     val customer_id = prefix + fooCounter.inc()
     val updateSession: Session = session
@@ -55,11 +55,7 @@ trait GatlingScenarioBuilder[DTO_IN] extends SessionParamNames with GatlingScena
   protected def dtoGenerate(session: Session)(implicit genFunction: String => TestCaseData[DTO_IN]): Session = {
     val customer_id = session(customerIdSessionName).as[String]
     val testCaseData = genFunction(customer_id)
-    val dto = testCaseData.data
-    //    val universalDto = UniversalDto(customer_id, Map(), Map(), Map())
-
-    val bytesUniversalDto = AvroUtil.encode[DTO_IN](dto, testingDataType.inputMeta.encoder, testingDataType.inputMeta.writer)
-
+    val bytesUniversalDto = AvroUtil.encode[DTO_IN](testCaseData.data, testingDataType.inputMeta.encoder, testingDataType.inputMeta.writer)
     session
       .set(bytesInputDtoSessionName, bytesUniversalDto)
       .set(testCaseName, testCaseData.caseName)
